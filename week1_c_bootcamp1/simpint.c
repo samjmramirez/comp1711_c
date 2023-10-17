f7698a6cd89a74cca451c290ceaff39ab04704bf
Samantha Ramirez <146263285+samjmramirez@users.noreply.github.com>
2023-10-17 14:18:49 +0000

    17.10.23_2

--- week1_c_bootcamp1/simpint.c
+++ week1_c_bootcamp1/simpint.c
@@ -4,5 +4,18 @@ int main()
 {
     float i, p, r, t;
 
-    printf("")
+    printf("Enter the principle amount (£): \n");
+    scanf("%f", &p);
+
+    printf("Enter the rate of interest(per annum): \n");
+    scanf("%f", &r);
+
+    printf("Enter the time period (years): \n");
+    scanf("%f", &t);
+
+    i = p * r * t;
+
+    printf("The simple interest is %f\n", i);
+
+    return 0;
 }
